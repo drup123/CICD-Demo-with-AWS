71dfcdba679e9cee0e30d01b5b6d8ce95ec7b562
Drup Patil <145984896+drup123@users.noreply.github.com>
2026-04-10 11:22:39 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import add
 class TestApp(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 6)  # ❌ Intentional bug (should be 5)
+        self.assertEqual(add(2, 3), 5)  # ❌ Intentional bug (should be 5)
 
 if __name__ == "__main__":
     unittest.main()
